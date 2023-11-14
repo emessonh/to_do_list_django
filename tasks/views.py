14e1dc262f517f7e3f04b6b907640c83ee67f753
emessonh <ehs4@aluno.ifal.edu.br>
2023-11-14 10:54:40 -0300

    iniciando layout

---
+++ tasks/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def helloWorld(request):
+    return HttpResponse('Hello World!')
+
+def taskList(request):
+    return render(request, 'tasks/list.html')
+
+
