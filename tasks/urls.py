9913766e7dfd3accd59cecc364356745e217c129
emessonh <ehs4@aluno.ifal.edu.br>
2023-11-14 14:08:39 -0300

    lendo registros do banco

--- tasks/urls.py
+++ tasks/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('helloworld/', views.helloWorld),
-    path('', views.taskList),
+    path('', views.taskList, name='task-list'),
+    path('task/<int:id>', views.taskView, name='task-view'),
 ]
