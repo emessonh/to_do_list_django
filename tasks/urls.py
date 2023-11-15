4070386c02ee22f89e725b1da69498ee3e41534f
emessonh <ehs4@aluno.ifal.edu.br>
2023-11-15 14:01:20 -0300

    adicionando task

--- tasks/urls.py
+++ tasks/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('helloworld/', views.helloWorld),
     path('', views.taskList, name='task-list'),
     path('task/<int:id>', views.taskView, name='task-view'),
+    path('newtask/', views.newTask, name='new-task'),
 ]
